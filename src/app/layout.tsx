4b5e88c46cc7cfee7181accc9a48496f9eeb2a38
Enkhbold Ganbold <enkhbold470@gmail.com>
2024-01-13 07:09:13 -0800

    title bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="./favicon_io/favicon.ico" />
-      <title></title>
       <body className="font-azonix">{children}</body>
     </html>
   );
