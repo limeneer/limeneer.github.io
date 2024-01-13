6a1c2d6d97d69476916afaf08f0d940a15ff8bf2
Enkhbold470 <enkhbold470@gmail.com>
2024-01-13 04:24:10 -0800

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="./favicon_io/favicon.ico" />
+      <title></title>
       <body className="font-azonix">{children}</body>
     </html>
   );
